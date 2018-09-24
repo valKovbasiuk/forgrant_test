6c24ed841f6b3c62db2bb0cba25fbf863299deb6
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-09-25 01:07:07 +0300

    numbers formatting

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,97 @@
 const cryptoCurrencies = ["BTC", "LTC", "ETH"];
+const currencySymbols = {
+  USD: "$",
+  GBP: "£",
+  RUB: "₽",
+  EUR: "€"
+};
 
-var selectedCurrencyValue = document
-  .querySelector("option[value][selected='selected']")
-  .getAttribute("value");
+function numberWithSpaces(x) {
+  let parts = (Math.round(x * 100) / 100)
+    .toFixed(2)
+    .toString()
+    .split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return parts.join(".");
+}
 
-cryptoCurrencies.forEach(function(cryptoCurrency) {
-  var request =
-    "https://apiv2.bitcoinaverage.com/indices/global/ticker/" +
-    cryptoCurrency +
-    selectedCurrencyValue;
+function priceChange(x) {
+  let parts = Math.round(x)
+    .toString()
+    .split(".");
+  return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
 
-  fetch(request)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      var elementList = Array.from(
-        document.getElementById(cryptoCurrency).querySelectorAll("td[data-set]")
-      ).map(item => item.getAttribute("data-set"));
+function isNegative(change) {
+  if (Number(change) < 0) {
+    return true;
+  } else {
+    false;
+  }
+}
 
-      elementList.forEach(function(datasetValue) {
-        if (datasetValue === "last") {
+function update() {
+  var selectedCurrencyValue = document
+    .querySelector("option[value][selected='selected']")
+    .getAttribute("value");
+
+  cryptoCurrencies.forEach(function(cryptoCurrency) {
+    var request =
+      "https://apiv2.bitcoinaverage.com/indices/global/ticker/" +
+      cryptoCurrency +
+      selectedCurrencyValue;
+
+    fetch(request)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        var elementList = Array.from(
           document
             .getElementById(cryptoCurrency)
-            .querySelector("[data-set='last']").textContent =
-            data[datasetValue];
-        } else {
-          document
-            .getElementById(cryptoCurrency)
-            .querySelector("[data-set='" + datasetValue + "']").textContent =
-            data.changes.price[datasetValue];
-        }
+            .querySelectorAll("td[data-set]")
+        ).map(item => item.getAttribute("data-set"));
+
+        elementList.forEach(function(datasetValue) {
+          if (datasetValue === "last") {
+            let price = data[datasetValue];
+            let priceSpaced =
+              currencySymbols[selectedCurrencyValue] + numberWithSpaces(price);
+
+            document
+              .getElementById(cryptoCurrency)
+              .querySelector("[data-set='last']").textContent = priceSpaced;
+          } else {
+            let change = data.changes.price[datasetValue];
+
+            if (!isNegative(change)) {
+              var changeDecor =
+                "+" +
+                priceChange(change) +
+                currencySymbols[selectedCurrencyValue];
+            } else {
+              var changeDecor =
+                priceChange(change) + currencySymbols[selectedCurrencyValue];
+            }
+
+            document
+              .getElementById(cryptoCurrency)
+              .querySelector(
+                "[data-set='" + datasetValue + "']"
+              ).textContent = changeDecor;
+
+            if (isNegative(change)) {
+              document
+                .getElementById(cryptoCurrency)
+                .querySelector("[data-set='" + datasetValue + "']")
+                .classList.add("field__value--negative");
+            }
+          }
+        });
+      })
+      .catch(err => {
+        // console.log("error while fetch JSON");
       });
-    })
-    .catch(err => {
-      console.log("error while fetch JSON");
-    });
-});
+  });
+}
+update();
